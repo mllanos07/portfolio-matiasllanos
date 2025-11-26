4e0b2edda96b97c75da97211776a65786d86590e
mllanos07 <d47001835@alumnos.itr.edu.ar>
2025-11-25 21:32:42 -0300

    Prepara para deploy en Railway

--- app.py
+++ app.py
@@ -338,5 +338,5 @@ def delete_project(proj_id):
 
 
 if __name__ == "__main__":
-    # para desarrollo local
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
